fb26b765e4d3471e85a00063b44f42b3ce7e9696
GoodGod <guskov.workakk@gmail.com>
2022-01-03 18:35:54 +0500

    Done

--- palindromNumbers/Program.cs
+++ palindromNumbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace palindromNumbers
 {
@@ -6,7 +7,30 @@ namespace palindromNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Solution.IsPalindrome(121));
+            Console.WriteLine(Solution.IsPalindrome(-121));
+            Console.WriteLine(Solution.IsPalindrome(263));
+        }
+    }
+
+    public static class Solution 
+    {
+        public static bool IsPalindrome(int x) 
+        {
+            if (x < 0 || (x % 10 == 0 && x != 0))
+                return false;
+
+            var reversedNumber = 0;
+            var number = x;
+            
+            while (number > 0)
+            {
+                reversedNumber *= 10;
+                reversedNumber += number % 10;
+                number /= 10;
+            }
+
+            return reversedNumber == x;
         }
     }
 }
