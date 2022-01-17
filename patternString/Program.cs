4fbca996bca229393786ebfc626fead7ec011bf9
Zuguki <guskov.zahar20@gmail.com>
2022-01-17 15:00:07 +0300

    Pattert String done

--- patternString/Program.cs
+++ patternString/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace patternString
 {
@@ -7,6 +9,18 @@ namespace patternString
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+    
+    public class Solution {
+        public bool WordPattern(string pattern, string str) 
+        {
+            var words = str.Split(' ');
+
+            return pattern.Length == words.Length && UniqueMapping(pattern, words) && UniqueMapping(words, pattern);
+
+            static bool UniqueMapping<T1, T2>(IEnumerable<T1> x, IEnumerable<T2> y)
+                => x.Zip(y).GroupBy(p => p.First).All(g => g.Distinct().Count() == 1);
         }
     }
 }
