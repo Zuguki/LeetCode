ded98d7ecce8d06d55c6723d154a24021e4f09bc
Zuguki <guskov.zahar20@gmail.com>
2022-01-17 15:01:01 +0300

    Add Two Numbers init

---
+++ addTwoNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace addTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
