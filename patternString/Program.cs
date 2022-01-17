6b61bd9ae54510a230032c0e63e4b9d807e612ee
Zuguki <guskov.zahar20@gmail.com>
2022-01-17 14:59:01 +0300

    pattertString init

---
+++ patternString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace patternString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
