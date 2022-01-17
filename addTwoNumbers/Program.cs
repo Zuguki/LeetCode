b220882b4b2ee1940a60945e8472e04e26a88701
Zuguki <guskov.zahar20@gmail.com>
2022-01-17 15:03:51 +0300

    Init ListNode

--- addTwoNumbers/Program.cs
+++ addTwoNumbers/Program.cs
@@ -2,11 +2,28 @@
 
 namespace addTwoNumbers
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sol = new Solution();
+            Console.WriteLine(sol.AddTwoNumbers(new ListNode(), new ListNode()));
+        }
+    }
+    
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            return new ListNode();
+        }
+    }
+
+    public class ListNode
+    {
+        public ListNode()
+        {
+            
         }
     }
 }
