f9bc3718cd0c077dc702200157988ddaec071bdf
Zuguki <guskov.zahar20@gmail.com>
2022-01-22 16:39:51 +0300

    Added TreeNode

--- sameTree/Program.cs
+++ sameTree/Program.cs
@@ -9,4 +9,18 @@ namespace sameTree
             Console.WriteLine("Hello World!");
         }
     }
+    
+    public class TreeNode 
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        
+        public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) 
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+}
 }
