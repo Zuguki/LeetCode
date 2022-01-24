c8ecde84dc8943896f3a1b04e2bf0cc9c54cb1e4
Zuguki <guskov.zahar20@gmail.com>
2022-01-24 19:31:28 +0300

    Done

--- detectCapital/Program.cs
+++ detectCapital/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Linq;
 
 namespace detectCapital
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sol = new Solution();
+            Console.WriteLine(sol.DetectCapitalUse("USA"));
+            Console.WriteLine(sol.DetectCapitalUse("FlaG"));
         }
+    }
+    
+    public class Solution 
+    {
+        public bool DetectCapitalUse(string word) => word.Where(char.IsUpper).Count() == 1 && char.IsUpper(word[0])
+                                                     || word.All(char.IsUpper)
+                                                     || word.All(char.IsLower);
     }
 }
