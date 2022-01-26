03022ebb5823629b7e98552fcc1c0ae460e89a5c
Zuguki <guskov.zahar20@gmail.com>
2022-01-26 17:06:21 +0300

    All Elements in Two Binary Search Trees init

---
+++ allElementsInTwoBinarySearchTrees/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace allElementsInTwoBinarySearchTrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
