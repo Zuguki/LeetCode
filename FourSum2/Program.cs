5b0aafb4db8ba377f86d1ce26017a81fea83e8fc
Zuguki <guskov.zahar20@gmail.com>
2022-02-03 18:04:42 +0300

    Added Solution

--- FourSum2/Program.cs
+++ FourSum2/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Linq;
 
 namespace FourSum2
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+    
+    public class Solution {
+        public int FourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4)
+        {
+            return 1;
         }
     }
 }
