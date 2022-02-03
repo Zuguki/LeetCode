12d7c850005ac2aa282dd70a6642474d59a703b6
Zuguki <guskov.zahar20@gmail.com>
2022-02-03 18:14:44 +0300

    Four Sum 2 done

--- FourSum2/Program.cs
+++ FourSum2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace FourSum2
@@ -14,7 +15,32 @@ namespace FourSum2
     public class Solution {
         public int FourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4)
         {
-            return 1;
+            var dict = new Dictionary<int, int>();
+            var result = 0;
+
+            foreach (var num1 in nums1)
+            {
+                foreach (var num2 in nums2)
+                {
+                    var sum = num1 + num2;
+                    if (dict.ContainsKey(sum))
+                        dict[sum]++;
+                    else
+                        dict.Add(sum, 1);
+                }
+            }
+
+            foreach (var num3 in nums3)
+            {
+                foreach (var num4 in nums4)
+                {
+                    var sum = num3 + num4;
+                    if (dict.TryGetValue(-sum, out var value))
+                        result += value;
+                }
+            }
+
+            return result;
         }
     }
 }
