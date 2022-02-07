28c8539d5869fa8b843023fbc0438e48b46edbd8
Zuguki <guskov.zahar20@gmail.com>
2022-02-07 20:18:01 +0300

    Find the diffenece done

--- findTheDifference/Program.cs
+++ findTheDifference/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Linq;
 
 namespace findTheDifference
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+    }
+    
+    public class Solution 
+    {
+        public char FindTheDifference(string s, string t)
+        {
+            foreach (var letter in t.Where(letter => !s.Contains(letter) 
+                                                     || t.Count(let => let == letter) 
+                                                     > s.Count(c => c == letter)))
+            {
+                return letter;
+            }
+
+            return ' ';
+        } 
     }
 }
