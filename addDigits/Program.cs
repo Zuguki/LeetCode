bcbae63c3fe8b3897a54860f6cb9aa1019d4aaba
Zuguki <guskov.zahar20@gmail.com>
2022-02-08 12:39:35 +0300

    Add Digits done (Faster tha 99.05%)

--- addDigits/Program.cs
+++ addDigits/Program.cs
@@ -2,11 +2,26 @@
 
 namespace addDigits
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+    
+    public class Solution 
+    {
+        public int AddDigits(int num)
+        {
+            var sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            return sum < 10 ? sum : AddDigits(sum);
         }
     }
 }
