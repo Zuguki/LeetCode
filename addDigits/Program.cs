e38b6b41099490ae7bc9e7fcca8155ed5eaabdda
Zuguki <guskov.zahar20@gmail.com>
2022-02-08 12:27:43 +0300

    Add digits init

---
+++ addDigits/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace addDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
