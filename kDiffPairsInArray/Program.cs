1ffd93c921ec236d73b912e9bb3f76ee71c2a65e
Zuguki <guskov.zahar20@gmail.com>
2022-02-09 18:11:37 +0300

    First sol

--- kDiffPairsInArray/Program.cs
+++ kDiffPairsInArray/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace kDiffPairsInArray
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sol = new Solution();
+            var nums = new[] {1,2,4,4,3,3,0,9,2,3};
+            var k = 3;
+
+            Console.WriteLine(sol.FindPairs(nums, k));
+        }
+    }
+    
+    public class Solution
+    {
+        public int FindPairs(int[] nums, int k)
+        {
+            var pairs = new List<(int, int)>();
+
+            for (var i = 0; i < nums.Length - 1; i++)
+            {
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    if (Math.Abs(nums[i] - nums[j]) == k 
+                        && !pairs.Contains((nums[i], nums[j])) && !pairs.Contains((nums[j], nums[i])))
+                        pairs.Add((nums[i], nums[j]));
+                }
+            }
+
+            return pairs.Count;
         }
     }
 }
