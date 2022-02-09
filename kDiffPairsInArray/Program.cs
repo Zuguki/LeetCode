7645388130d0c22bc8b287ae4f49c3bb818a96ea
Zuguki <guskov.zahar20@gmail.com>
2022-02-09 16:54:05 +0300

    K-diff Pairs in an Array init

---
+++ kDiffPairsInArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kDiffPairsInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
