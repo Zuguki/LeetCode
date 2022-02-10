a388dd8835cc1523cac8a53d05326c48059911f8
Zuguki <guskov.zahar20@gmail.com>
2022-02-10 15:05:21 +0300

    Added Solution class

--- subarraySumEqualsK/Program.cs
+++ subarraySumEqualsK/Program.cs
@@ -2,11 +2,19 @@
 
 namespace subarraySumEqualsK
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+    
+    public class Solution
+    {
+        public int SubarraySum(int[] nums, int k)
+        {
+            return 1;
         }
     }
 }
