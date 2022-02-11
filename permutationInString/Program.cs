9477e3ebd096eaec6750e900c3c368bf9a2e1caa
Zuguki <guskov.zahar20@gmail.com>
2022-02-11 14:21:50 +0300

    Done

--- permutationInString/Program.cs
+++ permutationInString/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace permutationInString
 {
@@ -6,7 +8,9 @@ namespace permutationInString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sol = new Solution();
+            var ans = sol.CheckInclusion("abc", "ccccbbbbaaaa");
+            Console.WriteLine(ans);
         }
     }
     
@@ -14,7 +18,31 @@ namespace permutationInString
     {
         public bool CheckInclusion(string s1, string s2)
         {
-            return true;
+            if (s1.Length > s2.Length)
+                return false;
+
+            var s1Hash = new int[26]; 
+            var s2Hash = new int[26];
+
+            for (var i=0; i<s1.Length; i++)
+            {
+                s1Hash[s1[i] - 97]++;
+                s2Hash[s2[i] - 97]++;
+            }
+
+            if (s1Hash.SequenceEqual(s2Hash)) 
+                return true; 
+            
+            for (var i=s1.Length; i<s2.Length; i++)
+            {   
+                s2Hash[s2[i] - 97]++;
+                s2Hash[s2[i - s1.Length] - 97]--;
+
+                if(s1Hash.SequenceEqual(s2Hash))
+                    return true; 
+            }
+            
+            return false;
         }
     }
 }
