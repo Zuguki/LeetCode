dfcdd80bb266db37784f2f5be34ca1e9c434ebf0
Zuguki <guskov.zahar20@gmail.com>
2022-02-14 08:30:01 +0300

    TreeNode class done

--- maximumDepthOfBinaryTree/Program.cs
+++ maximumDepthOfBinaryTree/Program.cs
@@ -2,11 +2,25 @@
 
 namespace maximumDepthOfBinaryTree
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+    
+    public class TreeNode 
+    {
+        public int Val;
+        public TreeNode Left;
+        public TreeNode Right;
+        
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null) 
+        {
+            Val = val;
+            Left = left;
+            Right = right;
         }
     }
 }
