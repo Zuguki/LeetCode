894a70d52b9aea3366546ea7e3c3604593f4e93a
Zuguki <guskov.zahar20@gmail.com>
2022-02-14 08:57:48 +0300

    Done

--- maximumDepthOfBinaryTree/Program.cs
+++ maximumDepthOfBinaryTree/Program.cs
@@ -6,21 +6,42 @@ namespace maximumDepthOfBinaryTree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sol = new Solution();
+            Console.WriteLine(sol.MaxDepth(null));
         }
     }
     
     public class TreeNode 
     {
-        public int Val;
-        public TreeNode Left;
-        public TreeNode Right;
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
         
         public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null) 
         {
-            Val = val;
-            Left = left;
-            Right = right;
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public class Solution
+    {
+        private int _counter = 0;
+        
+        public int MaxDepth(TreeNode root, int counter = 0)
+        {
+            if (counter == 0 && root is not null)
+                counter = 1;
+
+            if (root?.left != null)
+                MaxDepth(root.left, counter + 1);
+
+            if (root?.right != null)
+                MaxDepth(root.right, counter + 1);
+
+            _counter = counter > _counter ? counter : _counter;
+            return _counter;
         }
     }
 }
