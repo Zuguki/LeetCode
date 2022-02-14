d677aec0bad0165c5934be88d6b28b8420d01c4a
Zuguki <guskov.zahar20@gmail.com>
2022-02-14 08:27:07 +0300

    Maximum Depth of Binary Tree init

---
+++ maximumDepthOfBinaryTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace maximumDepthOfBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
