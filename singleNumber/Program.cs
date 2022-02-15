98363c17a979949f4c20f56ea41ce57339f2aca1
Zuguki <guskov.zahar20@gmail.com>
2022-02-15 17:10:32 +0300

    Done

--- singleNumber/Program.cs
+++ singleNumber/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace singleNumber
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+    
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            var numsDict = new Dictionary<int, int>();
+
+            foreach (var num in nums)
+            {
+                if (numsDict.ContainsKey(num))
+                    numsDict[num]++;
+                else
+                    numsDict.Add(num, 1);
+            }
+
+            return numsDict.First(item => item.Value == 1).Key;
         }
     }
 }
