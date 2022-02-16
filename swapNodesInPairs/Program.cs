f83c7669e5b0654e61892f6fc66d7965efb62e1f
Zuguki <guskov.zahar20@gmail.com>
2022-02-16 13:13:58 +0300

    Added List Node class;

--- swapNodesInPairs/Program.cs
+++ swapNodesInPairs/Program.cs
@@ -2,11 +2,22 @@
 
 namespace swapNodesInPairs
 {
-    class Program
+    internal static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+    }
+    
+    public class ListNode 
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val=0, ListNode next=null) 
+        {
+            this.val = val;
+            this.next = next;
+        } 
     }
 }
