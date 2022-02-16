a0770b6c1f0f4d8a44b591d7aaa46b1593d543f4
Zuguki <guskov.zahar20@gmail.com>
2022-02-16 13:12:45 +0300

    Swap Nodes in Pairs init

---
+++ swapNodesInPairs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace swapNodesInPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
