a033aef5bfdd9dd9c1c8c6c7fa1235dc6cdb09ac
Zuguki <guskov.zahar20@gmail.com>
2022-09-12 18:17:29 +0300

    FindShort

--- Tasks/Credit Card Mask/Program.cs
+++ Tasks/Credit Card Mask/Program.cs
@@ -1,21 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
-Console.WriteLine(DigPow.digPow(89, 1));
+Console.ReadKey();
 
-public class DigPow 
+public class Kata
 {
-    public static long digPow(int n, int p)
-    {
-        var strFromNumber = n.ToString();
-        var multiplyValue = p;
-        var resultValue = strFromNumber.Select(sym => int.Parse(sym.ToString()))
-            .Select(parseValue => (int) Math.Pow(parseValue, multiplyValue++)).Sum();
-
-        if (resultValue % n == 0)
-            return resultValue / n;
-        return -1;
-    }
+    public static int FindShort(string s) => s.Split(' ').Min(x => x.Length);
 }
