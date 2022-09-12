ba03d160cc1499fdbefbddaf8b399f52f6d8c2ac
Zuguki <guskov.zahar20@gmail.com>
2022-09-12 14:38:48 +0300

    Credi Card Mask

---
+++ Tasks/Credit Card Mask/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+var str = Kata.Maskify("Hello My Friends");
+Console.WriteLine(str);
+str = Kata.Maskify("He");
+Console.WriteLine(str);
+
+public static class Kata
+{
+    public static string? Maskify(string cc)
+    {
+        var sb = new StringBuilder();
+        for (var index = 0; index < cc.Length; index++)
+            sb.Append(cc.Length - index > 4 ? '#' : cc[index]);
+
+        return sb.ToString();
+    }
+}
