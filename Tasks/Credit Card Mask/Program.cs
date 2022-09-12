2f208032bec5ab5e2439c71cd513af17b189a661
Zuguki <guskov.zahar20@gmail.com>
2022-09-12 20:12:47 +0300

    Tests was added and Remove_char done

--- Tasks/Credit Card Mask/Program.cs
+++ Tasks/Credit Card Mask/Program.cs
@@ -2,29 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 
-Console.WriteLine(Kata.Solution(10));
 Console.ReadKey();
 
-public static class Kata
+public class Kata
 {
-    public static int Solution(int value)
-    {
-        if (value < 0)
-            return -1;
-
-        var result = 0;
-        for (var item = 0; item < value; item++)
-        {
-            if (item % 3 == 0 && item != 0)
-            {
-                result += item;
-                continue;
-            }
-
-            if (item % 5 == 0 && item != 0)
-                result += item;
-        }
-
-        return result;
-    }
+    public static string Remove_char(string s) => s.Substring(1, s.Length - 2);
 }
---
+++ Tasks/Credit Card Mask/Tests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Credit_Card_Mask;
+
+[TestFixture]
+public class Test
+{
+    [Test]
+    public void Test1()
+    {
+        StringAssert.AreEqualIgnoringCase("loquen", Kata.Remove_char("eloquent"));
+        StringAssert.AreEqualIgnoringCase("ountr", Kata.Remove_char("country"));
+        StringAssert.AreEqualIgnoringCase("erso", Kata.Remove_char("person"));
+        StringAssert.AreEqualIgnoringCase("lac", Kata.Remove_char("place"));
+        StringAssert.AreEqualIgnoringCase("", Kata.Remove_char("ok"));
+    }
+}
