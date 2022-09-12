e0052949a883c9e3a17ee626deeacda69772dde0
Zuguki <guskov.zahar20@gmail.com>
2022-09-12 18:17:43 +0300

    Solution done

--- Tasks/Credit Card Mask/Program.cs
+++ Tasks/Credit Card Mask/Program.cs
@@ -1,9 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
+Console.WriteLine(Kata.Solution(10));
 Console.ReadKey();
 
-public class Kata
+public static class Kata
 {
-    public static int FindShort(string s) => s.Split(' ').Min(x => x.Length);
+    public static int Solution(int value)
+    {
+        if (value < 0)
+            return -1;
+
+        var result = 0;
+        for (var item = 0; item < value; item++)
+        {
+            if (item % 3 == 0 && item != 0)
+            {
+                result += item;
+                continue;
+            }
+
+            if (item % 5 == 0 && item != 0)
+                result += item;
+        }
+
+        return result;
+    }
 }
