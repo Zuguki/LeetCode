46baf57e09073edca86246d89794942cebf91492
Zuguki <guskov.zahar20@gmail.com>
2022-09-15 22:42:20 +0300

    Good Solution with BigInteger

--- Tasks/Credit Card Mask/Program.cs
+++ Tasks/Credit Card Mask/Program.cs
@@ -1,45 +1,9 @@
 ﻿using System;
-using System.Linq;
-using System.Text;
+using System.Numerics;
 
 Console.ReadKey();
 
 public class Kata
 {
-    public static string? Add(string a, string b)
-    {
-        var sb = new StringBuilder();
-        var science = 0;
-        var counter = 0;
-        while (counter++ < Math.Max(a.Length, b.Length))
-        {
-            var aInt = 0;
-            var bInt = 0;
-            
-            if (a.Length < counter)
-                aInt = 0 + science > 0 ? science-- : 0;
-            else
-                aInt = int.Parse(a[^counter].ToString()) + (science > 0 ? science-- : 0);
-
-            if (b.Length >= counter)
-                bInt = int.Parse(b[^counter].ToString());
-            
-            if (aInt + bInt >= 10)
-            {
-                sb.Append((aInt + bInt) % 10);
-                science++;
-            }
-            else
-                sb.Append(aInt + bInt);
-        }
-
-        if (science > 0)
-            sb.Append(science);
-
-        var result = new StringBuilder();
-        foreach (var sym in sb.ToString().Reverse())
-            result.Append(sym);
-
-        return result.ToString();
-    } 
+    public static string? Add(string a, string b) => (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString();
 }
