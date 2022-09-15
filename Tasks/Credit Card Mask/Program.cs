09943d42ba2c53b0fd1fd73ae0ccb5c034ca2188
Zuguki <guskov.zahar20@gmail.com>
2022-09-15 22:40:20 +0300

    Bad solution

--- Tasks/Credit Card Mask/Program.cs
+++ Tasks/Credit Card Mask/Program.cs
@@ -1,105 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 Console.ReadKey();
 
-public class RomanNumerals
+public class Kata
 {
-    private static readonly Dictionary<char, int> _romanValues = new()
-    {
-        {'I', 1},
-        {'V', 5},
-        {'X', 10},
-        {'L', 50},
-        {'C', 100},
-        {'D', 500},
-        {'M', 1000}
-    };
-
-    public static string ToRoman(int n)
+    public static string? Add(string a, string b)
     {
         var sb = new StringBuilder();
-        var value = GetLetters(n, 'M');
-        sb.Append(value.Item1);
-        
-        value = GetLetters(value.Item2, 'C', 'M', true);
-        sb.Append(value.Item1);
-
-        value = GetLetters(value.Item2, 'D', 'M');
-        sb.Append(value.Item1);
-
-        value = GetLetters(value.Item2, 'C', 'D');
-        sb.Append(value.Item1);
-        
-        value = GetLetters(value.Item2, 'X', 'C', true);
-        sb.Append(value.Item1);
-
-        value = GetLetters(value.Item2, 'L', 'C');
-        sb.Append(value.Item1);
-
-        value = GetLetters(value.Item2, 'X', 'L');
-        sb.Append(value.Item1);
-
-        value = GetLetters(value.Item2, 'I', 'X', true);
-        sb.Append(value.Item1);
-
-        value = GetLetters(value.Item2, 'V');
-        sb.Append(value.Item1);
-
-        value = GetLetters(value.Item2, 'I', 'V');
-        sb.Append(value.Item1);
-        
-        return sb.ToString();
-    }
-
-    public static int FromRoman(string romanNumeral)
-    {
-        var number = 0;
-        for (var index = 0; index < romanNumeral.Length; index++)
+        var science = 0;
+        var counter = 0;
+        while (counter++ < Math.Max(a.Length, b.Length))
         {
-            if (index < romanNumeral.Length - 1
-                && _romanValues[romanNumeral[index]] >= _romanValues[romanNumeral[index + 1]]
-                || index == romanNumeral.Length - 1)
-                number += _romanValues[romanNumeral[index]];
+            var aInt = 0;
+            var bInt = 0;
+            
+            if (a.Length < counter)
+                aInt = 0 + science > 0 ? science-- : 0;
             else
+                aInt = int.Parse(a[^counter].ToString()) + (science > 0 ? science-- : 0);
+
+            if (b.Length >= counter)
+                bInt = int.Parse(b[^counter].ToString());
+            
+            if (aInt + bInt >= 10)
             {
-                number += _romanValues[romanNumeral[index + 1]] - _romanValues[romanNumeral[index]];
-                index++;
+                sb.Append((aInt + bInt) % 10);
+                science++;
             }
+            else
+                sb.Append(aInt + bInt);
         }
 
-        return number;
-    }
+        if (science > 0)
+            sb.Append(science);
 
-    private static (string, int) GetLetters(int value, char currentChar, char previewChar = default, bool needs9Check = false)
-    {
-        var str = string.Empty;
-        var currentValue = _romanValues[currentChar];
-        var counter = value / currentValue;
-        
-        if (counter == 0)
-            return (str, value);
-        
-        if (previewChar == default)
-        {
-            str = new string(currentChar, counter);
-            return (str, value % currentValue);
-        }
+        var result = new StringBuilder();
+        foreach (var sym in sb.ToString().Reverse())
+            result.Append(sym);
 
-        if (needs9Check && counter == 9)
-        {
-            str = currentChar + previewChar.ToString();
-            return (str, value % currentValue);
-        }
-        if (needs9Check)
-            return (str, value);
-
-        if (counter == 4 && !needs9Check)
-            str = currentChar + previewChar.ToString();
-        else
-            str = new string(currentChar, counter);
-        
-        return (str, value % currentValue);
-    }
+        return result.ToString();
+    } 
 }
