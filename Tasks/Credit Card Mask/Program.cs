3dd2b3967ca426a29680c0a90d0df9a4b0f59ce2
Zuguki <guskov.zahar20@gmail.com>
2022-09-16 07:49:49 +0300

    Immortal not done (Array dimensions exceded)

--- Tasks/Credit Card Mask/Program.cs
+++ Tasks/Credit Card Mask/Program.cs
@@ -1,9 +1,33 @@
 ﻿using System;
-using System.Linq;
+using System.Numerics;
 
 Console.ReadKey();
 
-public class Kata
+public static class Immortal
 {
-    public static int[] ArrayDiff(int[] a, int[] b) => a.Where(item => b.All(bItem => bItem != item)).ToArray();
+    public static bool Debug = false;
+
+    public static long ElderAge(long rows, long columns, long time, long maxTime)
+    {
+        var platform = GetPlatform(rows, columns, time);
+        return platform.Item2 % maxTime;
+    }
+
+    private static (long[][], long) GetPlatform(long rows, long columns, long time)
+    {
+        // var platform = new BigInteger[rows, columns];
+        var platform = new long[rows][];
+        var counter = 0L;
+        for (var row = 0; row < platform.GetLength(0); row++)
+        {
+            platform[row] = new long[columns];
+            for (var column = 0; column < platform[row].Length; column++)
+            {
+                platform[row][column] = time >= (row ^ column) ? 0 : (row ^ column) - time;
+                counter += platform[row][column];
+            }
+        }
+
+        return (platform, counter);
+    }
 }
--- Tasks/Credit Card Mask/Tests.cs
+++ Tasks/Credit Card Mask/Tests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace Credit_Card_Mask;
@@ -6,13 +7,21 @@ namespace Credit_Card_Mask;
 public class Tests
 {
     [Test]
-    public void SampleTest()
+    public void example()
     {
-        Assert.AreEqual(new int[] {2},       Kata.ArrayDiff(new int[] {1, 2},    new int[] {1}));
-        Assert.AreEqual(new int[] {2, 2},    Kata.ArrayDiff(new int[] {1, 2, 2}, new int[] {1}));
-        Assert.AreEqual(new int[] {1},       Kata.ArrayDiff(new int[] {1, 2, 2}, new int[] {2}));
-        Assert.AreEqual(new int[] {1, 2, 2}, Kata.ArrayDiff(new int[] {1, 2, 2}, new int[] {}));
-        Assert.AreEqual(new int[] {},        Kata.ArrayDiff(new int[] {},        new int[] {1, 2}));
-        Assert.AreEqual(new int[] {3}, Kata.ArrayDiff(new int[] {1, 2, 3}, new int[] {1, 2}));
+        Assert.AreEqual((long) 5, Immortal.ElderAge(8, 5, 1, 100));
+        Assert.AreEqual((long) 224, Immortal.ElderAge(8, 8, 0, 100007));
+        Assert.AreEqual((long) 11925, Immortal.ElderAge(25, 31, 0, 100007));
+        Assert.AreEqual((long) 4323, Immortal.ElderAge(5, 45, 3, 1000007));
+        Assert.AreEqual((long) 1586, Immortal.ElderAge(31, 39, 7, 2345));
+        Assert.AreEqual((long) 808451, Immortal.ElderAge(545, 435, 342, 1000007));
+        // You need to run this test very quickly before attempting the actual tests :)
+        Assert.AreEqual((long) 5456283, Immortal.ElderAge(28827050410L, 35165045587L, 7109602, 13719506));
+    }
+
+    [Test]
+    public void MyTests()
+    {
+        Console.WriteLine(2 ^ 1);
     }
 }
